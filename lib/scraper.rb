eead1216754fef6a39ce3d0a39f71a862882266d
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-06-16 02:02:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,15 +12,27 @@ class Scraper
         student_location = student.css('.student-location').text
         student_name = student.css('.student-name').text
         students << {name: student_name, location: student_location, profile_url: student_profile_link}
-      end
+    end
     end
     students
   end
 
 
-  def self.scrape_profile_page(profile_url)
-    
-  end
+  def self.scrape_profile_page(profile_slug)
+    student = {}
+    profile_page = Nokogiri::HTML(open(profile_slug))
+    links = profile_page.css(".social-icon-container").children.css("a").map { |el| el.attribute('href').value}
+    links.each do |link|
+      if link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      elsif link.include?("twitter")
+        student[:twitter] = link
+      else
+        student[:blog] = link
+    end
+    end
 
 end
 
